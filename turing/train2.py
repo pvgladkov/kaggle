4151dc6c6f5a310fa6c2e1fc0f904ed3724ad812
Pavel Gladkov <webgeist.xor@gmail.com>
2017-07-15 14:09:05 +0300

    t2

--- turing/train2.py
+++ turing/train2.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 import numpy as np
 import scipy.sparse as sp
@@ -9,119 +11,8 @@ from sklearn.metrics import roc_auc_score, roc_curve
 from xgboost.sklearn import XGBClassifier
 import logging
 
-
-df_train = pd.read_csv('data/train.txt', sep='\t')
-df_validate = pd.read_csv('data/validation.txt', sep='\t')
-df_test = pd.read_csv('data/test.txt', sep='\t')
-
-
-def train2(df_train, df_validate):
-    cv = CountVectorizer(token_pattern=r'\S+', ngram_range=(1, 3), min_df=10, binary=True, dtype=np.uint8)
-    
-    def repl(string):
-        return string.replace('@@ ', '')
-    
-    print "prep"
-    X_train = cv.fit_transform(map(repl, df_train.response))
-    y_train = df_train['human-generated'].values
-
-    X_validate = cv.transform(map(repl, df_validate.response))
-    y_validate = df_validate['human-generated'].values
-    
-    print "fit"
-    C = 0.01
-    svm = LinearSVC(penalty='l1', dual=False, C=C, random_state=55)
-    svm.fit(X_train, y_train)
-    y_pred = svm.decision_function(X_validate)
-    
-    return roc_auc_score(y_validate, y_pred)
-
-
-def train_c(df_train, df_validate, df_test):
-    cv = CountVectorizer(token_pattern=r'\S+', ngram_range=(1, 3), min_df=10, binary=True, dtype=np.uint8)
-    
-    print "prep"    
-    X_train = cv.fit_transform(df_train.response)
-    y_train = df_train['human-generated'].values
-    
-    print "fit"
-    C = 0.01
-    svm = LinearSVC(penalty='l1', dual=False, C=C, random_state=55)
-    svm.fit(X_train, y_train)
-    
-    print "calibrate"
-    X_validate = cv.transform(df_validate.response)
-    y_validate = df_validate['human-generated'].values
-    
-    cclf = CalibratedClassifierCV(svm, cv='prefit')
-    cclf.fit(X_validate, y_validate)
-    
-    X_test = cv.transform(df_test.response)
-    y_pred = cclf.predict_proba(X_test)
-    
-    return y_pred
-
-
-def train_c2(df_train, df_validate, df_test):
-    cv = CountVectorizer(token_pattern=r'\S+', ngram_range=(1, 3), min_df=10, binary=True, dtype=np.uint8)
-    
-    print "prep"    
-    df_all = pd.concat((df_train, df_validate))
-    
-    X_train = cv.fit_transform(df_all.response)
-    y_train = df_all['human-generated'].values
-    
-    print "fit"
-    C = 0.01
-    svm = LinearSVC(penalty='l1', dual=False, C=C, random_state=55)
-    
-    cclf = CalibratedClassifierCV(svm, cv=3)
-    cclf.fit(X_train, y_train)
-    
-    X_test = cv.transform(df_test.response)
-    y_pred = cclf.predict_proba(X_test)
-    
-    return y_pred[:, 1]
-
-
-def train3(df_train, df_validate, df_test):
-    cv = CountVectorizer(token_pattern=r'\S+', ngram_range=(1, 3), min_df=150, binary=True, dtype=np.uint8)
-    
-    print("train: {}, validate: {}".format(df_train.shape, df_validate.shape))
-    
-    print("copy and concat")
-    df_tmp = df_train.copy()
-    df_tmp['response'] = df_train['context']
-    df_tmp['human-generated'] = 1
-    df_train = pd.concat((df_train, df_tmp))
-    
-    print("train: {}, validate: {}".format(df_train.shape, df_validate.shape))
-    
-    print("fit & transform")
-    X_train = cv.fit_transform(df_train.response)
-    y_train = df_train['human-generated'].values
-    
-    print("fit")
-    C = 0.01
-    svm = LinearSVC(penalty='l1', dual=False, C=C, random_state=55)
-    svm.fit(X_train, y_train)
-    
-    print('validate')
-    X_validate = cv.transform(df_validate.response)
-    y_validate = df_validate['human-generated'].values
-    y_pred = svm.decision_function(X_validate)
-    print(roc_auc_score(y_validate, y_pred))
-    
-    print('test submit')
-    X_test = cv.transform(df_test.response)
-    y_pred = svm.decision_function(X_test)
-    df_res = pd.DataFrame()
-    df_res['id'] = df_test.id
-    df_res['human-generated'] = y_pred
-
-    df_res.to_csv('sub150.csv', index=False)
-    
-    return True
+first_second = re.compile(r'<first_speaker>(.*?)<second_speaker>', re.IGNORECASE)
+first_second = re.compile(r'<first_speaker>(.*?)<second_speaker>', re.IGNORECASE)
 
 
 def add_features(df, X):
@@ -138,22 +29,61 @@ def add_features(df, X):
     
     dogs_count_response = df.response.apply(lambda x: x.count('@'))
     dogs_count_context = df.context.apply(lambda x: x.count('@'))
-    
+
+    at_count_response = df.response.apply(lambda x: x.count('<at>'))
+    at_count_context = df.context.apply(lambda x: x.count('<at>'))
+
+    url_count_response = df.response.apply(lambda x: x.count('<url>'))
+    url_count_context = df.context.apply(lambda x: x.count('<url>'))
+
     context_words = df.context.apply(lambda x: len(x.replace('@@ ', '').split()) * 1.0)
     context_chars = df.context.apply(lambda x: len(x.replace('@@ ', '')) * 1.0)
     context_avg_word_len = pd.Series(np.divide(context_chars, context_words))
-    
+
     response_words = df.response.apply(lambda x: len(x.replace('@@ ', '').split()) * 1.0)
     response_chars = df.response.apply(lambda x: len(x.replace('@@ ', '')) * 1.0)
     response_avg_word_len = pd.Series(np.divide(response_chars, response_words))
-    
+
+    context_speakers_count = ((context_has_first > 0) * 1.0 +
+                              (context_has_second > 0) * 1.0 +
+                              (context_has_third > 0) * 1.0)
+
+    response_speakers_count = ((response_has_first > 0) * 1.0 +
+                               (response_has_second > 0) * 1.0 +
+                               (response_has_third > 0) * 1.0)
+
+    context_r_count = context_has_first + context_has_second + context_has_third
+    response_r_count = response_has_first + response_has_second + response_has_third
+
+    np_context_has_first = context_has_first.values
+    np_response_has_first = response_has_first.values
+    np_context_has_second = context_has_second.values
+    np_response_has_second = response_has_second.values
+    np_context_has_third = context_has_third.values
+    np_response_has_third = response_has_third.values
+
+    c1 = ((np_context_has_first > 0) & (np_response_has_first > 0) &
+          (np_context_has_second == 0) & (np_response_has_second == 0) &
+          (np_context_has_third == 0) & (np_response_has_third == 0))
+
+    c2 = ((np_context_has_first == 0) & (np_response_has_first == 0) &
+          (np_context_has_second > 0) & (np_response_has_second > 0) &
+          (np_context_has_third == 0) & (np_response_has_third == 0))
+
+    c3 = ((np_context_has_first == 0) & (np_response_has_first == 0) &
+          (np_context_has_second == 0) & (np_response_has_second == 0) &
+          (np_context_has_third > 0) & (np_response_has_third > 0))
+
+    context_response_same = pd.Series(c1 | c2 | c3)
+
     cols = [context_has_first, context_has_second, context_has_third, response_has_first,
             response_has_second, response_has_third, response_len, context_len,
-            dogs_count_response, dogs_count_context, context_words,
-            context_chars, context_avg_word_len, response_words, response_chars,
-            response_avg_word_len]
-    
-    return sp.hstack([X] + [m.values[:,None] for m in cols])
+            dogs_count_response, dogs_count_context, response_words, response_chars,
+            response_avg_word_len, context_speakers_count, response_speakers_count, context_r_count,
+            response_r_count, context_response_same, context_words, context_chars, context_avg_word_len,
+            at_count_response, at_count_context, url_count_context, url_count_response]
+
+    return sp.hstack([X] + [m.values[:, None] for m in cols])
 
 
 def train_xgb_search(df_train, df_validate, df_test):
@@ -170,10 +100,10 @@ def train_xgb_search(df_train, df_validate, df_test):
     X_validate = add_features(df_validate, X_validate)
     y_validate = df_validate['human-generated'].values
     
-    for d in [12, 15, 18]:
-        for lr in [0.3, 0.35, 0.4]:
-            for ne in [40, 50, 60]:
-                xgb = XGBClassifier(max_depth=d, learning_rate=lr, n_estimators=ne, nthread=30)
+    for d in [9]:
+        for lr in [0.3]:
+            for ne in [200]:
+                xgb = XGBClassifier(max_depth=d, learning_rate=lr, n_estimators=ne, nthread=40)
                 xgb.fit(X_train, y_train)
                 y_pred = xgb.predict_proba(X_validate)
                 score = roc_auc_score(y_validate, y_pred[:, 1])
@@ -192,7 +122,7 @@ def train_xgb(df_train, df_validate, df_test):
     y_train = df_all['human-generated'].values
     
     print('fit')
-    xgb = XGBClassifier(max_depth=15, learning_rate=0.4, n_estimators=60, nthread=30)
+    xgb = XGBClassifier(max_depth=15, learning_rate=0.3, n_estimators=60, nthread=40)
     xgb.fit(X_train, y_train)
     
     X_test = cv.transform(df_test.response)
@@ -202,19 +132,24 @@ def train_xgb(df_train, df_validate, df_test):
     df_res['id'] = df_test.id
     df_res['human-generated'] = y_pred[:, 1]
 
-    df_res.to_csv('sub_xgb_features_1.csv', index=False)
+    df_res.to_csv('sub_xgb_features_2.csv', index=False)
     
     return True
 
 
-print(train_xgb_search(df_train, df_validate, df_test))
+if __name__ == '__main__':
+    train = pd.read_csv('data/train.txt', sep='\t')
+    validate = pd.read_csv('data/validation.txt', sep='\t')
+    test = pd.read_csv('data/test.txt', sep='\t')
 
-#y_pred = train_c2(df_train, df_validate, df_test)
-#import joblib
-#joblib.dump(y_pred, 'y_pred.joblib')
-#joblib.dump(df_test.id, 'id.joblib')
-#df_res = pd.DataFrame()
-#df_res['id'] = df_test.id
-#df_res['human-generated'] = y_pred
+    print(train_xgb_search(train, validate, test))
 
-#df_res.to_csv('sub2.csv', index=False)
+    #y_pred = train_c2(df_train, df_validate, df_test)
+    #import joblib
+    #joblib.dump(y_pred, 'y_pred.joblib')
+    #joblib.dump(df_test.id, 'id.joblib')
+    #df_res = pd.DataFrame()
+    #df_res['id'] = df_test.id
+    #df_res['human-generated'] = y_pred
+
+    #df_res.to_csv('sub2.csv', index=False)
